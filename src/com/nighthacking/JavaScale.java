e308cdbd28ac07627e2311e3009fc820cf6e3ccd
Stephen Chin <steveonjava@gmail.com>
2015-08-03 20:31:50 -0700

    Fixed a typo

--- src/com/nighthacking/JavaScale.java
+++ src/com/nighthacking/JavaScale.java
@@ -1,7 +1,7 @@
 package com.nighthacking;
 
 import com.nighthacking.recipes.AeroPressCoffee;
-import com.nighthacking.recipe.CommandLineReciperRunner;
+import com.nighthacking.recipe.CommandLineRecipeRunner;
 import com.nighthacking.recipes.CoffeeCalculator;
 
 /**
@@ -10,7 +10,7 @@ import com.nighthacking.recipes.CoffeeCalculator;
 public class JavaScale {
 
   public static void main(String[] args) throws InterruptedException {
-    CommandLineReciperRunner runner = new CommandLineReciperRunner();
+    CommandLineRecipeRunner runner = new CommandLineRecipeRunner();
     runner.runRecipe(new AeroPressCoffee(CoffeeCalculator.STRONG));
     runner.close();
   }
